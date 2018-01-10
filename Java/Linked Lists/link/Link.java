1aa8dae23e9170d893096b34fb24e48576e5447e
sevresbabylone <cheryl.francais@gmail.com>
2018-01-10 22:36:47 +0800

    adds Link class

---
+++ Java/Linked Lists/link/Link.java
@@ -0,0 +1,12 @@
+package link;
+
+public class Link {
+    public Link next;
+    public int iData;
+    public Link (int i) {
+        iData = i;
+    }
+    public void displayLink () {
+        System.out.println("iData: " + iData);
+    }
+}
