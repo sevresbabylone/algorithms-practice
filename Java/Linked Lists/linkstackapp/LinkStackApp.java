8d1dd7a588d9972f081992b76fc7ea467b2334d6
sevresbabylone <cheryl.francais@gmail.com>
2018-02-05 06:21:50 +0800

    adds linkstackapp

---
+++ Java/Linked Lists/linkstackapp/LinkStackApp.java
@@ -0,0 +1,18 @@
+package linkstackapp;
+
+import linkstack.LinkStack;
+
+public class LinkStackApp {
+    public static void main(String[] args) {
+        LinkStack stack = new LinkStack();
+        stack.push(20);
+        stack.push(40);
+        stack.displayStack();
+        stack.push(60);
+        stack.push(80);
+        stack.displayStack();
+        stack.pop();
+        stack.pop();
+        stack.displayStack();
+    }
+}
