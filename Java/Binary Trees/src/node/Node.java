798dbffb2d3969a584163d23e2e2ce1a3bea0746
sevresbabylone <cheryl.francais@gmail.com>
2018-02-15 19:21:15 +0800

    adds node

--- Java/Binary Trees/src/node/Node.java
+++ Java/Binary Trees/src/node/Node.java
@@ -1,11 +1,16 @@
 package node;
 
 public class Node {
-    private int iData;
-    private double fData;
-    private Node leftChild;
-    private Node rightChild;
+    public int iData;
+    public double fData;
+    public Node leftChild;
+    public Node rightChild;
 
+    public Node (int i, double f) {
+        iData = i;
+        fData = f;
+        leftChild = rightChild = null;
+    }
     public void displayNode() {
         System.out.println("Node: " + iData + " " + fData);
     }
