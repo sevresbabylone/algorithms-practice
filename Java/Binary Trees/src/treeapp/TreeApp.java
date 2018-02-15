a6129a6da011311d49a042393ea13d2d74f213bd
sevresbabylone <cheryl.francais@gmail.com>
2018-02-15 19:21:24 +0800

    adds treeapp

--- Java/Binary Trees/src/treeapp/TreeApp.java
+++ Java/Binary Trees/src/treeapp/TreeApp.java
@@ -1,14 +1,15 @@
 package treeapp;
 
+import node.Node;
 import tree.Tree;
 
 public class TreeApp {
     public static void main(String[] args) {
-        Tree treeA = new Tree;
+        Tree treeA = new Tree();
         treeA.insert(12, 3.3);
         treeA.insert(13, 4.4);
         treeA.insert(14, 5.5);
-        node found = treeA.find(13);
+        Node found = treeA.find(treeA.root, 13);
         if (found != null) {
             System.out.println("Found the node with key 13");
         }
