efdf98ab919e5265933b945c443d425449e3faf8
sevresbabylone <cheryl.francais@gmail.com>
2018-02-23 23:44:51 +0800

    updates treeapp

--- Java/Binary Trees/src/treeapp/TreeApp.java
+++ Java/Binary Trees/src/treeapp/TreeApp.java
@@ -6,23 +6,25 @@ import tree.Tree;
 public class TreeApp {
     public static void main(String[] args) {
         Tree treeA = new Tree();
-        treeA.insert(12, 3.3);
-        treeA.insert(13, 4.4);
-        treeA.insert(14, 5.5);
-        treeA.insert(1, 5.5);
-        treeA.insert(2, 5.5);
-        treeA.insert(10, 5.5);
-        treeA.insert(123, 5.5);
-        treeA.insert(133, 5.5);
-        treeA.insert(140, 5.5);
+        treeA.insert(10, 3.3);
+        treeA.insert(5, 4.4);
+        treeA.insert(3, 5.5);
+        treeA.insert(7, 5.5);
+//        treeA.insert(2, 5.5);
+//        treeA.insert(10, 5.5);
+//        treeA.insert(123, 5.5);
+//        treeA.insert(133, 5.5);
+//        treeA.insert(140, 5.5);
 
         Node found = treeA.find(treeA.getRoot(), 13);
         if (found != null) {
             found.displayNode();
         }
-        Node minimum = treeA.minimum();
-        minimum.displayNode();
+
         Node maximum = treeA.maximum();
         maximum.displayNode();
+        treeA.delete(3);
+        Node minimum = treeA.minimum();
+        minimum.displayNode();
     }
 }
