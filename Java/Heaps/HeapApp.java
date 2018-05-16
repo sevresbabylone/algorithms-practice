dc461fa235bcd9c1f017cba62c8bd4f9873b5583
sevresbabylone <cheryl.francais@gmail.com>
2018-05-16 23:45:31 +0800

    adds heaps folder

---
+++ Java/Heaps/HeapApp.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class HeapApp {
+    public static void main(String[] args) throws IOException {
+        Heap heap1 = new Heap(31);
+        heap1.insert(70);
+        heap1.insert(40);
+        heap1.insert(50);
+        heap1.insert(20);
+        heap1.insert(60);
+        heap1.insert(100);
+        heap1.insert(80);
+        heap1.insert(30);
+        heap1.insert(10);
+        heap1.insert(90);
+    }
+}
---
+++ Java/Heaps/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    private int iData;
+
+    public Node(int key) {
+        iData = key;
+    }
+    public int getKey() {
+        return iData;
+    }
+    public void setKey(int key) {
+        iData = key;
+    }
+}
