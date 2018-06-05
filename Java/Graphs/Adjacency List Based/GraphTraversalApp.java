c0c54494a6c2b5cf52e494effad3499619525356
sevresbabylone <cheryl.francais@gmail.com>
2018-06-05 11:34:11 +0800

    adds adjacency list

---
+++ Java/Graphs/Adjacency List Based/GraphTraversalApp.java
@@ -0,0 +1,17 @@
+// Adjacency List of gtGraph
+// Vertex : List of adjacent vertexes
+// A : B -> C -> D -> E
+// B : F
+// C :
+// D : G
+// E :
+// F : H
+// G : I
+// H :
+// I :
+
+public class GraphTraversalApp {
+    public static void main(String[] args) {
+        
+    }
+}
