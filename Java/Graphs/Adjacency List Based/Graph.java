9db1082ae9244f4b5e631f32632eec91dc985372
sevresbabylone <cheryl.francais@gmail.com>
2018-06-06 22:01:32 +0800

    adds adjacency list based graph

--- Java/Graphs/Adjacency List Based/Graph.java
+++ Java/Graphs/Adjacency List Based/Graph.java
@@ -1,2 +1,54 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Graph {
+    private LinkedList<Integer>[] adjacencyList;
+    private Vertex[] vertexList;
+    private Stack<Integer> indexStack;
+    private Queue<Integer> indexQueue;
+
+    private int noOfVerts;
+    public Graph(int maxVerts) {
+        noOfVerts = 0;
+        vertexList = new Vertex[maxVerts];
+        indexStack = new Stack<Integer>();
+        indexQueue = new LinkedList<Integer>();
+        adjacencyList = new LinkedList[maxVerts];
+        // Create a new list for each vertex
+        // such that adjacent nodes can be stored
+        for(int i = 0; i < maxVerts ; i++){
+            adjacencyList[i] = new LinkedList<Integer>();
+        }
+    }
+    public void addVertex(char label) {
+        vertexList[noOfVerts++] = new Vertex(label);
+    }
+    public void addEdge(int source, int destination) {
+        adjacencyList[source].addLast(destination);
+        adjacencyList[destination].addLast(source);
+    }
+    public void displayAdjacencyList() {
+        for(int i = 0; i < noOfVerts; i++) {
+            Iterator<Integer> vertexIterator = adjacencyList[i].iterator();
+            System.out.print(vertexList[i].label + " : ");
+            while(vertexIterator.hasNext()) {
+                System.out.print(vertexList[vertexIterator.next()].label);
+                if (vertexIterator.hasNext()) System.out.print(" -> ");
+            }
+            System.out.println();
+        }
+    }
+    public void displayVertex(int vertexIndex) {
+        System.out.print(vertexList[vertexIndex].label);
+    }
+    public void dfs() {
+
+    }
+    public void bfs() {
+    }
+    public void mst() {
+
+    }
 }
--- Java/Graphs/Adjacency List Based/GraphTraversalApp.java
+++ Java/Graphs/Adjacency List Based/GraphTraversalApp.java
@@ -1,5 +1,6 @@
-// Adjacency List of gtGraph
+// Adjacency List of gtGraph (Non-directed)
 // Vertex : List of adjacent vertexes
+
 // A : B -> C -> D -> E
 // B : F
 // C :
@@ -12,6 +13,45 @@
 
 public class GraphTraversalApp {
     public static void main(String[] args) {
-        
+        Graph gtGraph = new Graph(9);
+        gtGraph.addVertex('a');
+        gtGraph.addVertex('b');
+        gtGraph.addVertex('c');
+        gtGraph.addVertex('d');
+        gtGraph.addVertex('e');
+        gtGraph.addVertex('f');
+        gtGraph.addVertex('g');
+        gtGraph.addVertex('h');
+        gtGraph.addVertex('i');
+        // a
+        gtGraph.addEdge(0, 1);
+        gtGraph.addEdge(0, 2);
+        gtGraph.addEdge(0, 3);
+        gtGraph.addEdge(0, 4);
+        // b
+        gtGraph.addEdge(1, 5);
+        // d
+        gtGraph.addEdge(3, 6);
+        // f
+        gtGraph.addEdge(5, 7);
+        // g
+        gtGraph.addEdge(6, 8);
+
+        gtGraph.displayAdjacencyList();
+        // Expected output
+        // A : B -> C -> D -> E
+        // B : A -> F
+        // C : A
+        // D : A -> G
+        // E : A
+        // F : B -> H
+        // G : D -> I
+        // H : F
+        // I : G
+
+        System.out.print("Depth-first Search order: ");
+        gtGraph.dfs();
+        // Expected output => abfhcdgie
+
     }
 }
