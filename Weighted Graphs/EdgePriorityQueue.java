0e0419706b180f3f9a1b4a0221bdb22e2ac09f07
sevresbabylone <cheryl.francais@gmail.com>
2018-06-09 17:30:19 +0800

    adds edge priority queue

---
+++ Weighted Graphs/EdgePriorityQueue.java
@@ -0,0 +1,44 @@
+public class EdgePriorityQueue {
+    private final int SIZE = 20;
+    private Edge[] queueArray;
+    private int size;
+    public EdgePriorityQueue() {
+        queueArray = new Edge[SIZE];
+        size = 0;
+    }
+    public void insert(Edge item) {
+        int i;
+        for (i = 0; i < size; i++) {
+            if (item.weight >= queueArray[i].weight) {
+                break; }
+        }
+        for (int j = size - 1; j > i; j--) {
+            queueArray[j+1] = queueArray[j];
+        }
+        queueArray[i] = item;
+        size++;
+    }
+    public Edge removeMinimum() {
+        return queueArray[--size];
+    }
+    public void removeN(int n) {
+        for(int j = n; j < size - 1; j++) {
+            queueArray[j] = queueArray[j+1];
+        }
+    }
+    public Edge peekMinimum() {
+        return queueArray[size-1];
+    }
+    public boolean isEmpty() {
+        return queueArray.length == 0;
+    }
+    // returns index of item with identical destination value
+    public int find (int destination) {
+        for (int i = 0; i < size; i++) {
+            if (queueArray[i].destination == destination) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
